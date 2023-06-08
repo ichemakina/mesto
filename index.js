5c8f9cb70f4e7146ee60953b9a66cb15b19e4b3e
ichemakina <themackinaira@yandex.ru>
2023-06-08 17:03:54 +0300

    Добавляет возможность редактирования профиля

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button')
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__subtitle');
+
+let form = document.querySelector('.popup__container');
+let nameInput = document.querySelector('input[name="name"]');
+let descriptionInput = document.querySelector('input[name="description"]');
 
 function openForm() {
     popup.classList.add('popup_opened');
@@ -12,3 +18,17 @@ function closeForm() {
 
 editButton.addEventListener('click', openForm);
 closeButton.addEventListener('click', closeForm);
+nameInput.value = profileName.textContent;
+descriptionInput.value = profileDescription.textContent;
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    let newName = nameInput.value;
+    let newDescription = descriptionInput.value;
+
+    profileName.textContent = newName;
+    profileDescription.textContent = newDescription;
+}
+
+form.addEventListener('submit', handleFormSubmit);
