a6c5e7718a78a4a719ab27ec23df80c655a8eb82
ichemakina <themackinaira@yandex.ru>
2023-06-09 14:12:54 +0300

    Рефакторинг

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,14 @@ let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__subtitle');
-let likes = document.querySelectorAll('.photo-grid__like');
 
-let form = document.querySelector('.popup__container');
-let nameInput = document.querySelector('input[name="name"]');
-let descriptionInput = document.querySelector('input[name="description"]');
+let form = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__field_type_name');
+let descriptionInput = document.querySelector('.popup__field_type_description');
 
 function openForm() {
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
     popup.classList.add('popup_opened');
 }
 
@@ -25,14 +26,10 @@ function handleFormSubmit(evt) {
 
     profileName.textContent = newName;
     profileDescription.textContent = newDescription;
+    closeForm();
 }
 
 editButton.addEventListener('click', openForm);
 closeButton.addEventListener('click', closeForm);
-nameInput.value = profileName.textContent;
-descriptionInput.value = profileDescription.textContent;
 
 form.addEventListener('submit', handleFormSubmit);
-likes.forEach(l => l.addEventListener('click', function () {
-    l.className = 'photo-grid__like_active';
-}))
